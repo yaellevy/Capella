2988af5a0891c450441bb78b5aef85834ccb386c
AlexSpradling <alexspradling@gmail.com>
2022-11-27 22:44:19 -0500

    Added init and setup.

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Capella",
+    version="1.0.2",
+    author="AlexSpradling",
+    author_email="alexspradling@gmail.com",
+    description="Capella Beta",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/AlexSpradling/Capellav1.1.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
